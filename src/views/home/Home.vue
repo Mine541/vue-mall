f8a4dd7e1fcf40bafb5aece3eae83533dcc5db9d
gwang <1319989287@qq.com>
2019-10-17 00:43:55 +0800

    first commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,165 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <tab-control :titles="['流行', '新款', '精选']"
+                 @tabClick="tabClick"
+                 ref="tabControl1"
+                 class="tab-control" v-show="isTabFixed"/>
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control :titles="['流行', '新款', '精选']"
+                   @tabClick="tabClick"
+                   ref="tabControl2"/>
+      <goods-list :goods="showGoods"/>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
+  import Scroll from "components/common/scroll/Scroll";
+
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import { getHomeMultidata, getHomeGoods } from "network/home";
+  import {itemImgListener, backTopMixin} from "common/mixin";
+
+  export default {
+    name: "Home",
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
+        currentType: 'pop',
+        tabOffsetTop: 0,
+        isTabFixed: false,
+        saveY: 0
+      }
+    },
+    components: {
+      NavBar,
+      TabControl,
+      GoodsList,
+      Scroll,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
+      }
+    },
+    created() {
+      this.getHomeMultidata()
+
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    mixins: [itemImgListener, backTopMixin],
+    mounted() {
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then( res=> {
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+
+          this.$refs.scroll.finishPullUp();
+        })
+      },
+
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+        this.$refs.tabControl1.currentIndex = index;
+        this.$refs.tabControl2.currentIndex = index;
+      },
+      swiperImageLoad() {
+        this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop;
+      },
+      contentScroll(position) {
+        this.listenShowBackTop(position);
+        this.isTabFixed = -(position.y) > this.tabOffsetTop
+      },
+      loadMore() {
+        this.getHomeGoods(this.currentType);
+      }
+    },
+    activated() {
+      this.$refs.scroll.scrollTo(0, this.saveY, 0);
+      this.$refs.scroll.refresh();
+    },
+    deactivated() {
+      this.saveY= this.$refs.scroll.getScrollY()
+      this.$bus.$off('itemImageLoad', this.itemImgListener)
+    }
+  }
+</script>
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+    height: 100vh;
+    position: relative;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .content {
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  .tab-control {
+    position: relative;
+    z-index: 9;
+  }
+
+</style>
